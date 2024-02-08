682e118694d7a18c9c4526ed67e410d9dde13256
shaam <msyamil.hamami@gmail.com>
2024-02-08 07:56:01 +0700

    Edit swagger ui

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,7 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerDocument, {
     customCssUrl:
-      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css",
+      "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.3/swagger-ui.css",
   })
 );
 
