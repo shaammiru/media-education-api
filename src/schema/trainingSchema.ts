1f4d6c908f416ad0589755e4b534a9f47d23cc69
shaammiru <msyamil.hamami@gmail.com>
2024-06-14 11:38:12 +0700

    feat: update training schema

--- src/schema/trainingSchema.ts
+++ src/schema/trainingSchema.ts
@@ -9,6 +9,10 @@ const trainingSchema = joi.object({
     .string()
     .valid(...eventStatus)
     .allow(null),
+  certificate: joi.string().allow(null),
+  syllabus: joi.string().allow(null),
+  urlExternalTitle: joi.string().allow(null),
+  urlExternal: joi.string().uri().allow(null),
   startTime: joi.date().iso().required(),
   endTime: joi.date().iso().required(),
   price: joi.number().positive().required(),
@@ -24,6 +28,10 @@ const trainingUpdateSchema = joi.object({
   title: joi.string(),
   description: joi.string(),
   status: joi.string().valid(...eventStatus),
+  certificate: joi.string().allow(null),
+  syllabus: joi.string().allow(null),
+  urlExternalTitle: joi.string().allow(null),
+  urlExternal: joi.string().uri().allow(null),
   startTime: joi.date().iso(),
   endTime: joi.date().iso(),
   price: joi.number().positive(),
