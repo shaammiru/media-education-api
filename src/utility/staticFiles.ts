7d0b7ef3951aee75d02dd1c8e513844ea9ad3b12
shaammiru <msyamil.hamami@gmail.com>
2024-07-16 14:45:01 +0700

    fix: create env for static dir

--- src/utility/staticFiles.ts
+++ src/utility/staticFiles.ts
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs/promises";
 import { v4 } from "uuid";
 
-const staticDirectory = path.join(__dirname, "../../api/uploads");
+const staticDirectory = path.join(__dirname, `../../${process.env.STATIC_DIR}`);
 
 const upload = async (file: Express.Multer.File, directory: string) => {
   const fileName = `${Date.now().toString()}-${v4()}.${
@@ -10,11 +10,11 @@ const upload = async (file: Express.Multer.File, directory: string) => {
   }`;
   const filePath = path.join(staticDirectory, directory, fileName);
   await fs.writeFile(filePath, file.buffer);
-  return `${process.env.HOST}/api/uploads/${directory}/${fileName}`;
+  return `${process.env.HOST}/${process.env.STATIC_DIR}/${directory}/${fileName}`;
 };
 
 const remove = async (url: string) => {
-  const fileName = url.split("/api/uploads/")[1];
+  const fileName = url.split(`/${process.env.STATIC_DIR}`)[1];
   const filePath = path.join(staticDirectory, fileName);
   await fs.unlink(filePath);
 };
